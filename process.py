792e0c38d2bac712916da76aab35680b2a2b06f7
Gayoung <38778937+Goinging98@users.noreply.github.com>
2021-12-17 23:40:30 -0500

    Update process.py

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import glob, os
 
-# Current directory
+# Check current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
@@ -10,14 +10,14 @@ current_dir = 'data/obj'
 # Percentage of images to be used for the test set
 percentage_test = 10;
 
-# Create and/or truncate train.txt and test.txt
+# Create and truncate train.txt and test.txt
 file_train = open('data/train.txt', 'w')
 file_test = open('data/test.txt', 'w')
 
-# Populate train.txt and test.txt
 counter = 1
 index_test = round(100 / percentage_test)
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpeg")):
+    # If the file format is different, we need to change
     title, ext = os.path.splitext(os.path.basename(pathAndFilename))
 
     if counter == index_test:
